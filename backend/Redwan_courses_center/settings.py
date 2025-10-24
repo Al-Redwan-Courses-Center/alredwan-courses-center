8ffd96cfe5a5de49ce084cbe854c5af7ad8debde
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-10-24 19:09:05 +0300

    feat: Initial project setup and user model
    - Added core django apps
    - Configured custom user model
    - Created setup_psql_dev.sql

--- backend/Redwan_courses_center/settings.py
+++ backend/Redwan_courses_center/settings.py
@@ -38,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "debug_toolbar",
+    "users",
+    "attendance",
+    "core",
+    "courses",
+    "exams",
+    "lectures",
+    "enrollments",
+    "payments",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +134,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "users.CustomUser"
