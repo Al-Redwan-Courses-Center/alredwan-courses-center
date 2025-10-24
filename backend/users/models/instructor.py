19f5659d52ce272c1deb83941ad38e2c85cac225
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-10-24 20:30:23 +0300

    docs(instructor): add detailed table structure documentation for instructors model, will implement class later

--- backend/users/models/instructor.py
+++ backend/users/models/instructor.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+'''
+Table instructors { 
+  id uuid [pk] 
+  user_id uuid [ref: - users.id] 
+  bio text 
+  monthly_salary decimal(10,2)
+  // instructor_type enum('supervisor','external') [default: 'external']
+  hire_date date
+  avg_rating decimal(3,2) [null] // Cached average from student ratings
+}
+
+'''
