802ee5b7f463c46e8f3fd24e7be4a7867ce9c3f5
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-10-25 10:00:34 +0300

    refactor(instructor): update instructor model documentation and improve field naming for clarity

--- backend/users/models/instructor.py
+++ backend/users/models/instructor.py
@@ -1,15 +1,37 @@
 from django.db import models
-
+from .user import _
 # Create your models here.
 '''
-Table instructors { 
-  id uuid [pk] 
-  user_id uuid [ref: - users.id] 
-  bio text 
-  monthly_salary decimal(10,2)
-  // instructor_type enum('supervisor','external') [default: 'external']
-  hire_date date
-  avg_rating decimal(3,2) [null] // Cached average from student ratings
-}
-
+Module for Instructor model that represents an instructor user profile
 '''
+
+
+class Instructor(models.Model):
+    '''
+    Instructor model that represents an instructor user
+    '''
+    type_choices = [
+        ('supervisor', 'Supervisor'),
+        ('external', 'External'),
+    ]
+    user = models.OneToOneField(
+        'users.CustomUser', on_delete=models.CASCADE, related_name='instructor_profile')
+    bio = models.TextField(null=True, blank=True)
+    monthly_salary = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(max_length=512, null=True, blank=True)
+
+    nid_number = models.CharField(_("National ID number"), max_length=15)
+    avg_students_rating = models.DecimalField(
+        max_digits=3, decimal_places=2, null=True, blank=True)
+    avg_admins_rating = models.DecimalField(
+        max_digits=3, decimal_places=2, null=True, blank=True)
+
+    type = models.CharField(
+        max_length=20,
+        choices=type_choices,
+        default='external',
+    )
+
+    def __str__(self):
+        """String representation of the Instructor."""
+        return f"{self.user.first_name} {self.user.last_name}"
