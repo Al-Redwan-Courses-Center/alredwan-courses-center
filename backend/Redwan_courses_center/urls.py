4c1f1ff303a643fe673d7f18f345b650fd8cbaf6
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-10-28 18:51:36 +0300

    feat(images): add utility functions for image validation, resizing, and optimization

--- backend/Redwan_courses_center/urls.py
+++ backend/Redwan_courses_center/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + debug_toolbar_urls()
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
