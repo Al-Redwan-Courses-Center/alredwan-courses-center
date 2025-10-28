ddd04c5a79f26b13d139fefd63f1c13bc21248fa
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-10-28 19:09:51 +0300

    feat(users): update instructor model
    - Added image upload paths
    - Implemented image size validation
    - Added nid image field
    - Changed image field to ImageField

--- backend/users/models/instructor.py
+++ backend/users/models/instructor.py
@@ -1,9 +1,19 @@
 from django.db import models
+
+from core.utils.image_utils import validate_image_size
 from .user import _
 # Create your models here.
 '''
 Module for Instructor model that represents an instructor user profile
 '''
+
+
+def nid_upload_path(instance, filename):
+    return f"instructors/{instance.user.id}/nid/{filename}"
+
+
+def instructor_upload_path(instance, filename):
+    return f"instructors/{instance.id}/{filename}"
 
 
 class Instructor(models.Model):
@@ -18,9 +28,19 @@ class Instructor(models.Model):
         'users.CustomUser', on_delete=models.CASCADE, related_name='instructor_profile')
     bio = models.TextField(null=True, blank=True)
     monthly_salary = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.URLField(max_length=512, null=True, blank=True)
+    nid_image = models.ImageField(
+        upload_to=nid_upload_path,
+        validators=[validate_image_size],
+        blank=True, null=True
+    )
+    image = models.ImageField(
+        upload_to=instructor_upload_path,
+        validators=[validate_image_size],
+        default='defaults/user_default.png',
+        blank=True,
+        null=True,
+    )
 
-    nid_number = models.CharField(_("National ID number"), max_length=15)
     avg_students_rating = models.DecimalField(
         max_digits=3, decimal_places=2, null=True, blank=True)
     avg_admins_rating = models.DecimalField(
