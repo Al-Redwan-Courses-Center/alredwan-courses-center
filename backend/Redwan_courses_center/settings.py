800fe279e025f15ce49845dd86aa18af7e067919
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-12-17 16:10:51 +0200

    ✨ feat(settings): integrate environment variables for configuration
    - use django-environ to load settings from environment variables
    - configure SECRET_KEY, DEBUG, ALLOWED_HOSTS using environment variables
    - enable flexible database configuration via environment variables
    - add localhost and 0.0.0.0 to INTERNAL_IPS for broader access
    - add django-environ and psycopg2-binary to requirements.txt

--- backend/Redwan_courses_center/settings.py
+++ backend/Redwan_courses_center/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l91jto*@&@j*o3#02!x=)$hj_1ue87fd)(6v52dg06!5aqd^yc'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
@@ -84,13 +87,21 @@ ASGI_APPLICATION = "core.asgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+         'ENGINE': 'django.db.backends.{}'.format(
+             os.getenv('DATABASE_ENGINE', 'sqlite3')
+         ),
+         'NAME': os.getenv('DATABASE_NAME', 'polls'),
+         'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+         'PORT': os.getenv('DATABASE_PORT', 5432),
+     }
 }
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
+    "localhost",
+    "0.0.0.0"
     # ...
 ]
 
