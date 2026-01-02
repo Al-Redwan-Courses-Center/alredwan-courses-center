2b08fd4846e4722905aa983093a680f07abe35f4
eyadfattah23 <eyadabdelfattah4@gmail.com>
2026-01-03 01:20:32 +0200

    Remove unnecessary comments from docker-entrypoint.sh for cleaner code

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Source - https://stackoverflow.com/a
-# Posted by Louis Barranqueiro, modified by community. See post 'Timeline' for change history
-# Retrieved 2025-12-28, License - CC BY-SA 3.0
-
 # Collect static files
 echo "Collect static files"
 python manage.py collectstatic --noinput
