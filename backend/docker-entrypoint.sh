54b4ee2a2db0c7b78ffa90185a3ea50d4194a195
eyadfattah23 <eyadabdelfattah4@gmail.com>
2026-01-05 16:53:05 +0200

    Update docker-entrypoint.sh to use python3 and add makemigrations step

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Collect static files
 echo "Collect static files"
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 # Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
