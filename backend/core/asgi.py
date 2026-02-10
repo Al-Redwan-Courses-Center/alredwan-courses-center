515124b62657d5fcd6b9f0668d8c297b39f63b75
eyadfattah23 <eyadabdelfattah4@gmail.com>
2026-02-10 03:27:21 +0200

    refactor(asgi): reorder imports

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -4,11 +4,8 @@ ASGI config for Redwan_courses_center project.
 
 Supports both HTTP and WebSocket protocols.
 """
-import attendance.urls
 import os
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'Redwan_courses_center.settings')
@@ -16,6 +13,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+# Import after Django is configured
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import attendance.urls
 
 
 application = ProtocolTypeRouter({
