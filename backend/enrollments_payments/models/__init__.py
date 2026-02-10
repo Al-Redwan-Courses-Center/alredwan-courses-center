c3642a86544e96b6cedd975c9befc37c6d0710c5
eyadfattah23 <eyadabdelfattah4@gmail.com>
2026-02-10 03:28:34 +0200

    feat(enrollments_payments): add URL routing for enrollments and create serializers for enrollment-related models

--- backend/enrollments_payments/models/__init__.py
+++ backend/enrollments_payments/models/__init__.py
@@ -1,3 +1,4 @@
-from .enrollment_request import EnrollmentRequest
-from .enrollment import Enrollment
-from .payment import Payment, RefundRequest
+from .enrollment_request import EnrollmentRequest, EnrollmentRequestStatus
+from .enrollment_request import PaymentMethod as EnrollmentRequestPaymentMethod
+from .enrollment import Enrollment, EnrollmentStatus
+from .payment import Payment, RefundRequest, PaymentStatus, PaymentMethod
