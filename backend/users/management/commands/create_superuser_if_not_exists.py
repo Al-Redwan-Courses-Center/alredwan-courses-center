e4ef042050c30bbefbfbf1160b27b2daf0ff5844
eyadfattah23 <eyadabdelfattah4@gmail.com>
2026-02-12 17:13:26 +0200

    feat(users): update superuser creation to use first and last name from environment variables

--- backend/users/management/commands/create_superuser_if_not_exists.py
+++ backend/users/management/commands/create_superuser_if_not_exists.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from decouple import config
+from datetime import date
 
 
 class Command(BaseCommand):
@@ -11,7 +12,8 @@ class Command(BaseCommand):
         
         phone = config("DJANGO_SUPERUSER_PHONE", default=None)
         password = config("DJANGO_SUPERUSER_PASSWORD", default=None)
-        name = config("DJANGO_SUPERUSER_NAME", default="Admin")
+        first_name = config("DJANGO_SUPERUSER_FIRST_NAME", default="Admin")
+        last_name = config("DJANGO_SUPERUSER_LAST_NAME", default="User")
         
         if not phone or not password:
             self.stdout.write(
@@ -30,7 +32,10 @@ class Command(BaseCommand):
         User.objects.create_superuser(
             phone_number1=phone,
             password=password,
-            first_name=name,
+            first_name=first_name,
+            last_name=last_name,
+            dob=date(1990, 1, 1),  # Default DOB for admin
+            gender="male",  # Default gender for admin
         )
         
         self.stdout.write(
