c43b14684b575a144eb2cd51bc99ca8255a52cc3
Mohamed-Mos-aad <ma3268787@gmail.com>
2026-05-12 01:09:35 +0300

    feat: implement edit and delete child functionality with refactored form

---
+++ frontend/src/app/dashboard/(parent)/my-children/[childId]/edit/page.tsx
@@ -0,0 +1,29 @@
+import { getChildById } from "@/actions/user";
+import AddChildForm from "@/components/dashboard/parent/AddChildForm";
+import { notFound } from "next/navigation";
+import { protect } from "@/actions/auth";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ childId: string }>;
+}) {
+  await protect(["parent"]);
+  const { childId } = await params;
+  
+  const child = await getChildById(childId);
+
+  if (!child) {
+    return notFound();
+  }
+
+  return (
+    <div className="px-16 pt-15 flex flex-col gap-10 h-full overflow-auto pb-20">
+      <h3 className="text-olive-700 font-medad text-6xl">
+        تعديل بيانات الطفل
+      </h3>
+
+      <AddChildForm initialData={child} />
+    </div>
+  );
+}
