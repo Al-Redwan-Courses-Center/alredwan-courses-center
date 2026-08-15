8b6d63c8baa8cff69ecca151de32bcd0bd8f6482
Insanerest <157520929+insanerest@users.noreply.github.com>
2026-08-15 22:27:46 +0300

    fix: Update import path for cn & remove a developer note

--- frontend/src/shadcn/components/ui/search-input.tsx
+++ frontend/src/shadcn/components/ui/search-input.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Search } from "lucide-react";
 
 import { Input } from "./input";
-import { cn } from "@/lib/utils"; // تأكد من مسار الـ utils
+import { cn } from "../../../lib/utils";
 
 interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   containerClassName?: string;
@@ -21,7 +21,7 @@ export const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
           containerClassName,
         )}
       >
-        <Search className="h-5 w-5 shrink-0 text-olive-300" />
+        <Search className="text-olive-300 h-5 w-5 shrink-0" />
 
         <Input
           id={id}
